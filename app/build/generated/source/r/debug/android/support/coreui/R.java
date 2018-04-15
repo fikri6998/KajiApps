c194ea2fcf4f8bbb4ae939739cf814e64123659a
fikri6998 <fikri6998@gmail.com>
2018-04-15 22:49:58 +0700

    menambahkan halaman sharing kajian

--- app/build/generated/source/r/debug/android/support/coreui/R.java
+++ app/build/generated/source/r/debug/android/support/coreui/R.java
@@ -72,35 +72,35 @@ public final class R {
         public static final int async = 0x7f080020;
         public static final int blocking = 0x7f080024;
         public static final int chronometer = 0x7f08002d;
-        public static final int forever = 0x7f08004b;
-        public static final int icon = 0x7f08004f;
-        public static final int icon_group = 0x7f080050;
-        public static final int info = 0x7f080054;
-        public static final int italic = 0x7f080055;
-        public static final int line1 = 0x7f08005a;
-        public static final int line3 = 0x7f08005b;
-        public static final int normal = 0x7f080068;
-        public static final int notification_background = 0x7f080069;
-        public static final int notification_main_column = 0x7f08006a;
-        public static final int notification_main_column_container = 0x7f08006b;
-        public static final int right_icon = 0x7f080076;
-        public static final int right_side = 0x7f080077;
-        public static final int tag_transition_group = 0x7f0800a1;
-        public static final int text = 0x7f0800a2;
-        public static final int text2 = 0x7f0800a3;
-        public static final int time = 0x7f0800a9;
-        public static final int title = 0x7f0800aa;
+        public static final int forever = 0x7f08004c;
+        public static final int icon = 0x7f080050;
+        public static final int icon_group = 0x7f080051;
+        public static final int info = 0x7f080055;
+        public static final int italic = 0x7f080056;
+        public static final int line1 = 0x7f08005b;
+        public static final int line3 = 0x7f08005c;
+        public static final int normal = 0x7f08006a;
+        public static final int notification_background = 0x7f08006b;
+        public static final int notification_main_column = 0x7f08006c;
+        public static final int notification_main_column_container = 0x7f08006d;
+        public static final int right_icon = 0x7f080079;
+        public static final int right_side = 0x7f08007a;
+        public static final int tag_transition_group = 0x7f0800a5;
+        public static final int text = 0x7f0800a6;
+        public static final int text2 = 0x7f0800a7;
+        public static final int time = 0x7f0800ad;
+        public static final int title = 0x7f0800ae;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f09000a;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a0029;
-        public static final int notification_action_tombstone = 0x7f0a002a;
-        public static final int notification_template_custom_big = 0x7f0a0031;
-        public static final int notification_template_icon_group = 0x7f0a0032;
-        public static final int notification_template_part_chronometer = 0x7f0a0036;
-        public static final int notification_template_part_time = 0x7f0a0037;
+        public static final int notification_action = 0x7f0a002a;
+        public static final int notification_action_tombstone = 0x7f0a002b;
+        public static final int notification_template_custom_big = 0x7f0a0032;
+        public static final int notification_template_icon_group = 0x7f0a0033;
+        public static final int notification_template_part_chronometer = 0x7f0a0037;
+        public static final int notification_template_part_time = 0x7f0a0038;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0c004a;
--- app/src/main/java/mobivision/kajiapps/SharingActivity.java
+++ app/src/main/java/mobivision/kajiapps/SharingActivity.java
@@ -13,5 +13,6 @@ public class SharingActivity extends AppCompatActivity {
     }
 
     public void Launchsharingkajian(View view) {
+
     }
 }
