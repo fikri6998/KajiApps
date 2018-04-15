54198c2f5bd549e282b4a98876aafe46b224cd99
fikri6998 <fikri6998@gmail.com>
2018-04-15 22:43:13 +0700

    menambahkan halaman sharing kajian

---
+++ app/src/main/java/mobivision/kajiapps/SharingActivity.java
@@ -0,0 +1,17 @@
+package mobivision.kajiapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SharingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharing);
+    }
+
+    public void Launchsharingkajian(View view) {
+    }
+}
