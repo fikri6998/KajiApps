0c91fda857917da10b6fbc9d4227125b6d836d99
fikri6998 <fikri6998@gmail.com>
2018-04-23 00:07:38 +0700

    menambahkan halaman simpan jadwal kajian

---
+++ app/src/main/java/mobivision/kajiapps/SimpanJadwal.java
@@ -0,0 +1,13 @@
+package mobivision.kajiapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpanJadwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simpan_jadwal);
+    }
+}
